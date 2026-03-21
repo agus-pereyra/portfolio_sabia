f9e3ed1e12e6f2a1d1aae19aca25419d2a9f0688
Agustín Pereyra <apereyra@udesa.edu.ar>
2026-03-21 03:47:46 -0300

    Home Random Grid + Videos

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,7 @@
-from .models import Collaborator, Collection, Picture
+from .models import Collection, Picture, Video
 from django.views.generic import DetailView, ListView
 from django.shortcuts import render
+import random
 
 def about(request):
     return render(request, 'about.html')
@@ -21,9 +22,19 @@ def collections_list(request):
         'active_year': year,
     })
 
-def get_random_pictures(request):
-    random_pictures = Picture.objects.all().order_by('?')[:15]
-    return render(request, 'portfolio/includes/picture_grid_items.html', {'random_pictures': random_pictures})
+def get_random_media(request):
+    pictures = list(Picture.objects.all().order_by('?')[:12])
+    videos = list(Video.objects.all().order_by('?')[:3])
+
+    for p in pictures:
+        p.media_type = 'picture'
+    for v in videos:
+        v.media_type = 'video'
+        
+    random_media = pictures + videos
+    random.shuffle(random_media)
+
+    return render(request, 'portfolio/includes/picture_grid_items.html', {'random_media': random_media})
 
 class CollectionDetailView(DetailView):
     model = Collection
@@ -44,7 +55,19 @@ class HomeView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['collections_all'] = Collection.objects.all().order_by('-captured_at')
-        context['random_pictures'] = Picture.objects.all().order_by('?')[:12]
+        
+        pictures = list(Picture.objects.all().order_by('?')[:12])
+        videos = list(Video.objects.all().order_by('?')[:3])
+
+        for p in pictures:
+            p.media_type = 'picture'
+        for v in videos:
+            v.media_type = 'video'
+            
+        random_media = pictures + videos
+        random.shuffle(random_media)
+
+        context['random_media'] = random_media
         return context
 
     def get_queryset(self):
