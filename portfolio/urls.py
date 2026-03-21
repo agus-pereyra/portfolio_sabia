ffc8f734ed5b288b22ab27de1120c37c27445372
Agustín Pereyra <apereyra@udesa.edu.ar>
2026-03-21 02:48:44 -0300

    Home Random Grid

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CollectionDetailView, HomeView, about, collections_list
+from .views import CollectionDetailView, HomeView, about, collections_list, get_random_pictures
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('albums/', collections_list, name='collections_list'),
     path('albums/detail/<slug:slug>/', CollectionDetailView.as_view(), name='collection_detail'),
     path('about/', about, name='about'),
+    path('get-random-pics/', get_random_pictures, name='get_random_pics')
 ]
